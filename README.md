bf332295e78a474a3bc50e8a4698efb39a47dd9a
ms <>
2024-10-09 15:24:42 +0200

    update

--- README.md
+++ README.md
@@ -2,13 +2,38 @@
 module "azure_app_gateway" {
   source = "git::git@github.com:michalswi/ag.git?ref=main"
 
-  enable_ag = true
-
-  rg_name               = "my-resource-group"
   location              = "East US"
-  ag_frontend_subnet_id = "todo"
-  ag_static_pip_id      = "todo"
   app_service_fqdn      = ["myappservice.azurewebsites.net"]
+
+  # [rg exists]
+  rg_name               = "my-resource-group"
+  # [rg doesn't exist]
+  /*
+  create_rg             = true
+  */
+
+  # [subnet exists]
+  frontend_subnet_id = "todo"
+  # [subnet doesn't exists / vnet does exist]
+  /*
+  vnet_name = "todo"
+  create_subnet         = true
+  subnet_address_prefixes = ["10.10.1.0/24"]
+  */
+  # [subnet doesn't exists / vnet doesn't exist]
+  /*
+  create_vnet = true
+  vnet_address_space    = ["10.0.0.0/16"]
+  create_subnet         = true
+  subnet_address_prefixes = ["10.10.1.0/24"]
+  */
+  
+  # [pip exists]
+  static_pip_id      = "todo"
+  # [pip doesn't exist]
+  /*
+  create_pip = true
+  */
 
   tags = {
     Environment = "dev"
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "enable_ag" {
-  default = false
+  default = true
   type    = bool
 }
 
@@ -14,26 +14,92 @@ variable "name" {
   default     = "1ad"
 }
 
-variable "rg_name" {
-  description = "The name of the resource group to create the Application Gateway in."
-  type        = string
-}
-
 variable "location" {
   description = "The Azure region where the resources will be created."
   type        = string
 }
 
-variable "ag_frontend_subnet_id" {
+# rg
+variable "rg_name" {
+  description = "The name of an existing resource group to create the Application Gateway in."
+  type        = string
+  default     = ""
+}
+
+variable "create_rg" {
+  description = "Whether to create a new resource group or use an existing one."
+  type        = bool
+  default     = false
+}
+
+# vnet
+variable "create_vnet" {
+  description = "Whether to create a new vnet or use an existing one."
+  type        = bool
+  default     = false
+}
+
+variable "vnet_address_space" {
+  description = "Address space for the new VNET."
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+# subnet
+variable "vnet_name" {
+  description = "The name of an existing vnet."
+  type        = string
+  default     = ""
+}
+
+variable "frontend_subnet_id" {
   description = "Application Gateway frontend subnet id."
 }
 
-variable "ag_static_pip_id" {
+variable "create_subnet" {
+  description = "Whether to create a new subnet or use an existing one."
+  type        = bool
+  default     = false
+}
+
+variable "subnet_address_prefixes" {
+  description = "Address space for the new subnet."
+  type        = list(string)
+  default     = ["10.10.1.0/24"]
+}
+
+# pip
+variable "create_pip" {
+  description = "Whether to create a new static public ip or use an existing one."
+  type        = bool
+  default     = false
+}
+
+variable "static_pip_id" {
   description = "Application Gateway static public IP address id."
 }
 
+# ag
 variable "app_service_fqdn" {
   description = "The list of FQDNs of the App Service backend."
-  type        = set(string)
+  type        = list(string)
   default     = []
+}
+
+variable "sku_name" {
+  description = " The Name of the SKU to use for this Application Gateway."
+  type        = string
+  default     = "Standard_v2"
+}
+
+variable "sku_tier" {
+  description = "The Tier of the SKU to use for this Application Gateway."
+  type        = string
+  default     = "Standard_v2"
+}
+
+variable "sku_capacity" {
+  description = "The Capacity of the SKU to use for this Application Gateway."
+  type        = number
+  default     = 2
 }
