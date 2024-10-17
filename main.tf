7455391883e7193cfb83740741d1ca6cbab7325d
ms <>
2024-10-17 09:58:33 +0200

    update

--- main.tf
+++ main.tf
@@ -37,22 +37,16 @@ locals {
   }
 }
 
-# RBAC or MI - todo
+# RBAC
 resource "azurerm_role_assignment" "this" {
   principal_id         = azurerm_application_gateway.this.identity[0].principal_id
   role_definition_name = "Key Vault Certificates User"
   scope                = local.key_vault_id
 }
-# resource "azurerm_user_assigned_identity" "this" {
-#   name                = "${local.name}-mig"
-#   resource_group_name = local.rg_name
-#   location            = local.location
-#   tags                = local.tags
-# }
 
-# ssl certificates
+# ssl certificate
 data "azurerm_key_vault_certificate" "this" {
-  for_each     = { for cert in local.certificate_refs : cert => cert }
+  for_each     = length(local.certificate_refs) > 0 ? { for cert in local.certificate_refs : cert => cert } : {}
   key_vault_id = local.key_vault_id
   name         = each.value
 }
@@ -122,7 +116,7 @@ resource "azurerm_application_gateway" "this" {
     frontend_ip_configuration_name = local.frontend_public_ip_config
     frontend_port_name             = local.frontend_ports.https.name
     protocol                       = "Https"
-    # ssl_certificate_name = <todo>
+    # ssl_certificate_name = ... - todo
     # firewall_policy_id = azurerm_web_application_firewall_policy.this.id - todo
 
   }
@@ -139,7 +133,6 @@ resource "azurerm_application_gateway" "this" {
     # priority = 10/100?  - todo
   }
 
-  # todo - MI or RBAC
   identity {
     type = "SystemAssigned"
   }
@@ -147,26 +140,18 @@ resource "azurerm_application_gateway" "this" {
   # waf - todo
   # firewall_policy_id = azurerm_web_application_firewall_policy.this.id
 
-  # ssl_certificate - todo
+  # ssl certificate - todo
   dynamic "ssl_certificate" {
     for_each = data.azurerm_key_vault_certificate.this
 
     content {
-      name                = ssl_certificate.key
-      key_vault_secret_id = ssl_certificate.value.secret_id
+      name = ssl_certificate.key
+      # key_vault_secret_id = ssl_certificate.value.secret_id
+      key_vault_secret_id = try(ssl_certificate.value.secret_id, "")
     }
   }
 
-  # dynamic "ssl_certificate" {
-  #   for_each = { for cert in local.ssl_certificate : cert.name => cert.key_vault_secret_id }
-
-  #   content {
-  #     name = ssl_certificate.key
-  #     key_vault_secret_id = ssl_certificate.value
-  #   }
-  # }
-
-  # todo
+  # probe - todo
   # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/application_gateway#host
   # probe {
   # }
