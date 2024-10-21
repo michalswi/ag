ccb054c19efe0617c1ecefbddad112767d83f930
ms <>
2024-10-21 10:10:14 +0200

    update

--- main.tf
+++ main.tf
@@ -155,13 +155,6 @@ resource "azurerm_application_gateway" "this" {
     }
 
   }
-  # ssl_certificate {
-  #   name = try(data.azurerm_key_vault_certificate.this["ag-ssl-cert"].name, "default")
-  #   key_vault_secret_id = try(
-  #     data.azurerm_key_vault_certificate.this["ag-ssl-cert"].secret_id,
-  #     data.azurerm_key_vault_certificate.this["default"].secret_id
-  #   )
-  # }
 
   # todo
   # connect frontend and backend pool
@@ -185,9 +178,22 @@ resource "azurerm_application_gateway" "this" {
   # waf - todo
   # firewall_policy_id = azurerm_web_application_firewall_policy.this.id
 
-  # probe - todo
+  # todo
   # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/application_gateway#host
   # probe {
+  #   name                = "probe_${local.name}"
+  #   interval            = 30
+  #   protocol            = "Https"
+  #   path                = "/"
+  #   timeout             = 30
+  #   unhealthy_threshold = 3
+  #   minimum_servers     = 0
+  #   # host                                      = <todo>
+  #   # pick_host_name_from_backend_http_settings = false
+
+  #   match {
+  #     status_code = ["200-499"]
+  #   }
   # }
 
   tags = local.tags
@@ -203,5 +209,64 @@ resource "azurerm_application_gateway" "this" {
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/web_application_firewall_policy
 # resource "azurerm_web_application_firewall_policy" "this" {
 #   name                = "platform_base_firewall_policy"
-#   (...)
+#   resource_group_name = local.rg_name
+#   location            = local.location
+
+#   custom_rules {
+#     name      = "Rule1"
+#     priority  = 1
+#     rule_type = "MatchRule"
+
+#     match_conditions {
+#       match_variables {
+#         variable_name = "RemoteAddr"
+#       }
+
+#       operator           = "IPMatch"
+#       negation_condition = false
+#       match_values       = ["192.168.1.0/24", "10.0.0.0/24"]
+#     }
+
+#     action = "Block"
+#   }
+
+#   policy_settings {
+#     enabled                     = true
+#     mode                        = "Prevention"
+#     request_body_check          = true
+#     file_upload_limit_in_mb     = 100
+#     max_request_body_size_in_kb = 128
+#   }
+
+#   managed_rules {
+#     exclusion {
+#       match_variable          = "RequestHeaderNames"
+#       selector                = "x-company-secret-header"
+#       selector_match_operator = "Equals"
+#     }
+#     exclusion {
+#       match_variable          = "RequestCookieNames"
+#       selector                = "too-tasty"
+#       selector_match_operator = "EndsWith"
+#     }
+
+#     managed_rule_set {
+#       type    = "OWASP"
+#       version = "3.2"
+#       rule_group_override {
+#         rule_group_name = "REQUEST-920-PROTOCOL-ENFORCEMENT"
+#         rule {
+#           id      = "920300"
+#           enabled = true
+#           action  = "Log"
+#         }
+
+#         rule {
+#           id      = "920440"
+#           enabled = true
+#           action  = "Block"
+#         }
+#       }
+#     }
+#   }
 # }
